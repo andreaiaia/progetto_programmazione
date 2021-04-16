f4e6aeec6e8fabdf87ae6f8e2397d307c557cafe
andreaiaia <andreaiaia@hotmail.it>
2021-04-16 18:36:42 +0200

    update mappa.h

--- src/progetto/src/Mappa.cpp
+++ src/progetto/src/Mappa.cpp
@@ -8,52 +8,63 @@
 #include "Mappa.h"
 
 Mappa::Mappa() {
-	head = nuovo_schermo() ;
-	head->prev = NULL ;
-	head->next = NULL ;
+	for(int j=0; j<x; j++) {
+		for(int i=0; i<y; i++) {
+			if(j==1) schermo[j][i]='=' ;
+			if(j!=1) schermo[j][i]=' ' ;
+		}
+	}
+	piattaforme();
 }
 
-scena* Mappa::nuovo_schermo() {
-	scena*p= new scena ;
-	for(int j=0; j<x; j++) {
-				for(int i=0; i<y; i++) {
-					if(j==1)p->schermo[j][i]='=' ;
-					if(j!=1)p->schermo[j][i]=' ' ;
-				}
-			}
+void Mappa::piattaforme() {
 	int num_piatt = num_random(x/5)+2 ;                       // num_piatt= 2 o 3 (con x minimo 12 e y minimo 40)
-		int h_prec=1 ;
-		int o_prec=1 ;
-		int l_prec ;
-		for(int i=1; i<=num_piatt; i++) {                     // genera n piattaforme
-			if(h_prec<x-3) {
+	int h_prec=1 ;
+	int o_prec=1 ;
+	int l_prec ;
+	for(int i=1; i<=num_piatt; i++) {                     // genera n piattaforme
+		if(h_prec<x-3) {
 			int altezza=3+h_prec ;                            // altezza minima=3
 			int origine=num_random(y/5)+2;                    // origine minima=1
 			int lunghezza=num_random(y/8)+6;
-			while ((origine>=o_prec-2 && origine<=o_prec+l_prec+2) || origine + lunghezza > y-3 || (origine+lunghezza>=o_prec-2 && origine+lunghezza<=o_prec+l_prec+2))
-				  {
-				  origine=num_random(y/2)+y/2 ;               // cambia origine se necessario
-				  }
-
-			genera_piattaforma(p->schermo, altezza, origine, lunghezza) ;
+			while ((origine>=o_prec-2 && origine<=o_prec+l_prec+2) || origine + lunghezza > y-3
+					|| (origine+lunghezza>=o_prec-2 && origine+lunghezza<=o_prec+l_prec+2)) {
+				 origine=num_random(y/2)+y/2 ;               // cambia origine se necessario
+			}
+			genera_piattaforma(altezza, origine, lunghezza) ;
 			l_prec=lunghezza ;
 			h_prec=altezza ;
 			o_prec=origine ;
-			}
 		}
-	return *p ;
+	}
 }
 
-void Mappa::genera_piattaforma(char output[][y], int h, int o, int l) {
+void Mappa::genera_piattaforma(int h, int o, int l) {
 	int altezza=h ;
 	int origine=o ;
 	int lunghezza=l ;
-	output[altezza][origine]='('; //inizio
+	schermo[altezza][origine] = '('; //inizio
 	for(int j=origine+1; j<origine+lunghezza; j++) {
-		output[altezza][j]='='; //centro
-	  	  }
-	  	  output[altezza][origine+lunghezza]=')'; //fine
+		schermo[altezza][j] = '='; //centro
 	}
+	schermo[altezza][origine+lunghezza]=')'; //fine
+}
+
+/*void Mappa::popola(char figura, int d) {
+	int offset = 0;
+	if (d < 5) offset = 5-d;
+
+	int altezza = num_random(x) + offset;
+	int ordinata = num_random(y);
+
+	while ((schermo[altezza-1][ordinata] != '=') || (ordinata == 0 || ordinata == 1) || (schermo[altezza][ordinata] != '*')) {
+	    altezza = num_random(x) + offset;
+	    ordinata = num_random(y);
+	}
+	schermo[altezza][ordinata] = figura;
+
+	// Va creata una variabile di tipo nemico/oggetto nella quale salvare le coordinate del suddetto nemico/oggetto
+}*/
 
 int Mappa::num_random(int max) {
 	srand( time(0) ) ;
@@ -61,4 +72,11 @@ int Mappa::num_random(int max) {
 	return n ;
 }
 
-// ╔ ═ ╗ ¤ ♥
+char Mappa::contenuto(int a, int b) {
+	if (a > x) a = x;
+	else if (a < 0) a = 1;
+	if (b > y) b = y;
+	else if (b < 0) b = 0;
+
+	return schermo[a][b];
+}
--- src/progetto/src/Mappa.h
+++ src/progetto/src/Mappa.h
@@ -4,52 +4,26 @@
  *  Created on: 6 mar 2021
  *      Author: Simone
  */
-
 #include <iostream>
 #include <cstring>
 #include <cstdlib>
 #include <ctime>
 #include "Oggetto.h"
 #include "Cuore.h"
-
 using namespace std ;
 
 #define x 10
 #define y 10
 
-struct scena {
-	char schermo[x][y] ;
-	scena * prev ;
-	scena * next ;
-};
-
 class Mappa {
 protected:
-	scena * head ;
-
-	int difficolta = 6;
-/*
-	// Variabili per tenere traccia della map generation
-	int nuova_piattaforma = 4;
-	int altitudine = 0;
-*/
+	char schermo[x][y] ;
 
 public:
 	Mappa();
 
-	scena* nuovo_schermo();
-	int num_random(int max) ;
-	void genera_piattaforma(char output[x][y], int altezza, int o, int l) ;
-
-/*
- * facciamo una bilista al cui interno memorizziamo tutte le "colonne" che compongono la mappa,
- * implementiamo metodi per randomizzare un numero 0 o 1 per decidere se fare iniziare una piattaforma o no
- * uno per decidere quanti oggetti fare apparire e uno per decidere la lunghezza delle piattaforme
- * si deve anche decidere se tenere in considerazione l'altezza di queste piattaforme rispetto al personaggio
- * e decidere se impedire al personaggio di raggiungere piattaforme troppo alte
- * in caso potremmo anche fare apparire piattaforme in base all'altezza delle precedenti, mettendole
- * sempre più a salire.
- */
-
-
+	void piattaforme();
+	int num_random(int) ;
+	void genera_piattaforma(int, int, int) ;
+	char contenuto(int, int);
 };
